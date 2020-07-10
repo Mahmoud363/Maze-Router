84b95ffeb21bbd0f41efe240e3516c38f78b4fcd
IsmaelElsharkawi <61902007+IsmaelElsharkawi@users.noreply.github.com>
2020-07-10 23:09:57 +0200

    Update inputParser.cpp

--- inputParser.cpp
+++ inputParser.cpp
@@ -41,8 +41,25 @@ int main()
 				<< "and it is found at position "
 				<< match.position(0) << endl;
 			i++;
-			string parse = match.str(0);
+			//Extracting the details for each pin without the brackets
+			string parse = match.str(0).substr(1, match.str(0).size()-2);
+			//Entering the details of the pin into the struct pin
+			stringstream ss(parse); 
+			pin newpin;
+			for(int i=0;i<3;i++) { 
+				string substr; 
+				getline(ss, substr, ',' ); 
+				switch(i){
+					case 0:
+						newpin.layer= atoi(substr.c_str());
+					case 1:
+						newpin.x= atoi(substr.c_str());
+					case 2:
+						newpin.y= atoi(substr.c_str());
+				}
+			} 
 
+			cout<< newpin.layer<<"/"<<newpin.x<<"/"<<newpin.y<<endl;
 
 			// suffix to find the rest of the string. 
 			subject = match.suffix().str();
