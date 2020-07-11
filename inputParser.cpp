788144ca57bb308d8e049f00af187f3cc3df8822
Mahmud Elsayed <38327235+Mahmoud363@users.noreply.github.com>
2020-07-11 19:23:10 +0200

    prev version

--- inputParser.cpp
+++ inputParser.cpp
@@ -13,11 +13,25 @@ struct pin
 	string id;
 };
 
+string strip_spaces(string str) {
+	str.erase(remove(str.begin(), str.end(), ' '), str.end());
+	return str;
+}
+
+vector<string> split_commas(string str) {
+	str += ',';
+	vector<string> buffer;
+	string tmp;
+	while (!str.empty()) {
+		tmp = str.substr(str.find(',')+1);
+		buffer.push_back(tmp);
+		str.erase(str.begin(), str.end() + (str.find(','));
+	}
+	return buffer;
+}
+
 int main()
 {
-	string subject("n1 (1, 10, 20) (2, 30, 50) (1, 5 ,1000) "
-		"n2 (1, 10, 20) (2, 30, 50) (1, 5 ,1000)");
-
 	vector<vector<string>> parsedNet;
 	string sourcePath;
 	cin >> sourcePath;
@@ -28,10 +42,13 @@ int main()
 
 	// Template instantiations for 
 	// extracting the matching pattern. 
-	
+	int netCounter = 1;
+
 	string line;
 	getline(read, line);
 	while (!read.eof()) {
+		vector<string> net;
+		vector<string> pinValues;
 		smatch match;
 		regex r("\\((.*?)\\)");
 		string subject = line;
@@ -41,7 +58,9 @@ int main()
 				<< "and it is found at position "
 				<< match.position(0) << endl;
 			i++;
-			string parse = match.str(0);
+			string parse = match.str(0).substr(1, match.str(0).size()-2);
+			parse = strip_spaces(parse);
+			pinValues = split_commas(parse);
 
 
 			// suffix to find the rest of the string. 
