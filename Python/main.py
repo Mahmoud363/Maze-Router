c97dc2396b65fb015680e61051fef49bbe48a3a8
IsmaelElsharkawi <61902007+IsmaelElsharkawi@users.noreply.github.com>
2020-07-16 05:21:50 +0200

    Update main.py
    There are two problems, the scale is too small to appear. The second problem is that in a certain layer, if two wires are adjacent, but parallel, they will appear as a single wire.

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,6 @@
 import sys
+from PIL import Image, ImageDraw
+
 
 from inputPaser import inputParser
 from A_Star import *
@@ -12,6 +14,24 @@ input_parser.parseFile(path)
 input_parser.createGrid()
 
 paths = astar(input_parser.pinsGrid, input_parser.nets)
+
+img = []
+pixelImage = []
+index = input_parser.layers
+for i in range(index):
+    img.append(Image.new('RGB', (1000, 1000), color = (256, 256, 256)) )
+    draw = ImageDraw.Draw(img[i])
+    pixelImage.append( img[i].load() )
+
+for  patharray in paths: 
+
+    for path in (patharray):
+        (templayer, tempy, tempx) = path
+        pixelImage[templayer][tempx, tempy]= (0,0,0,255)
+
+for i in range(index):
+    text = "layer"+str(i+1)+".png"
+    img[i].save(text)
 
 print(paths)
 
