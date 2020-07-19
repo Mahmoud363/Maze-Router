211b94cab59c1f38f5243a583dd7ee3b8d18adcb
IsmaelElsharkawi <61902007+IsmaelElsharkawi@users.noreply.github.com>
2020-07-19 22:14:01 +0200

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -26,22 +26,20 @@ paths = astar(input_parser.pinsGrid, input_parser.nets)
 
 img = []
 pixelImage = []
-x=[]
-y=[]
-z=[]
+
 index = input_parser.layers
 for i in range(index):
     img.append(Image.new('RGB', (1000, 1000), color = (256, 256, 256)) )
     draw = ImageDraw.Draw(img[i])
     pixelImage.append( img[i].load() )
-    x.append( random.randint(100,255) )
-    y.append(random.randint(100,255) )
-    z.append(random.randint(100,255) )
+  
 for  patharray in paths: 
-
+    x= random.randint(100,255) 
+    y=random.randint(100,255) 
+    z=random.randint(100,255) 
     for path in (patharray):
         (templayer, tempy, tempx) = path
-        pixelImage[templayer][tempx, tempy]= ( x[templayer-1], y[templayer-1], z[templayer-1])
+        pixelImage[templayer][tempx, tempy]= ( x,y,z)
 
 for i in range(index):
     text = "layer"+str(i+1)+".png"
